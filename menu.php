8d5328f7495bf7a4904343bc4788bec1cc7c6eca
yuva <yuva@yuva-VGN-CS17G-R>
2012-11-21 11:50:49 +0530

    -added menu page and order page

---
+++ menu.php
@@ -0,0 +1,106 @@
+<?php 
+
+if($_POST)
+{
+	$name = $_POST['username'];
+	$passwd = $_POST['passwd'];
+	$mailid = $_POST['mailid'];
+	$address = $_POST['address'];
+	$phone = $_POST['phone'];
+}
+?>
+
+<html>
+	<head>
+		<title>
+			Home page
+		</title>
+		<style type="text/css">
+			.head{
+				padding-left:20px;
+			}
+			.menuitems{
+				padding-left:400px;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="head">
+		<img src="restaurant_header.jpg" width="100%" height="35%" />
+		<h1>
+			Welcome <?php echo $name; ?>
+		</h1>
+		</div>
+		<div class="menuitems">
+		<form name="menu" action="order.php" method="post" >
+		<h2>Menu  Items</h2>
+		<table>
+			<tr>
+				<td>
+					Dish   :
+				</td>
+				<td>
+					<select multiple="multiple" name="dish">
+  						<option value="chapathi">chapathi</option>
+  						<option value="Dosa">Dosa</option>
+  						<option value="Baati">Baati</option>
+  						<option value="Dal">Dal</option>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Quantity   :
+				</td>
+				<td>
+					<select name="dish_quantity">
+					<?php 
+						for($i=1;$i<=10;$i++)
+						{
+  							echo "<option value='$i'>$i</option>";
+						}
+					?>
+					</select> 
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Ice flavor :
+				</td>
+				<td>
+					<input type="radio" name="ice_flavor" value="Venilla">Venilla
+					<input type="radio" name="ice_flavor" value="Strawberry">Strawberry
+					<input type="radio" name="ice_flavor" value="Butter">Butter
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Quantity   :
+				</td>
+				<td>
+					<select name="ice_quantity">
+					<?php 
+						for($i=1;$i<=10;$i++)
+						{
+  							echo "<option value='$i'>$i</option>";
+						}
+					?>
+					</select> 
+				</td>
+			</tr>
+			<tr>
+				<td>
+					Toppings :
+				</td>
+				<td>
+					<input type="checkbox" name="toppings" value="nuts">Nuts
+					<input type="checkbox" name="toppings" value="dryfruits">Dry Fruits					
+				</td>
+			</tr>
+		</table>
+		<br>
+		<input type="submit" name="menusubmit" value="Order"/>
+		</form>
+		</div>
+	</body>
+</html>
---
+++ order.php
@@ -0,0 +1,73 @@
+<!-- Page for ordered items -->
+<?php
+
+if($_POST)
+{
+	//Item details posted from menu page
+	$dish_name = $_POST['dish'];
+	$dish_quantity = $_POST['dish_quantity'];
+	$ice_flavor = $_POST['ice_flavor'];
+	$ice_quantity = $_POST['ice_quantity'];
+	$toppings = $_POST['toppings'];
+	
+}
+
+?>
+
+<html>
+<head>
+<title>Order Page</title>
+<!-- Style for table head -->
+<style type="text/css">
+th {
+	background-color: lightblue;
+}
+</style>
+</head>
+<body>
+	<table border="1">
+		<tr>
+			<th>Dish</th>
+			<th>Quantity</th>
+			<th>Total</th>
+		</tr>
+		<tr>
+			<td><?php echo $dish_name; ?>
+			</td>
+			<td><?php echo $dish_quantity; ?>
+			</td>
+			<td><?php echo $dish_quantity * 20; ?>
+			</td>
+		</tr>
+		<tr>
+			<td><?php echo $ice_flavor; ?>
+			</td>
+			<td><?php echo $ice_quantity; ?>
+			</td>
+			<td><?php echo $ice_quantity * 15; ?>
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<?php 
+				 if($toppings == "nuts")
+					echo "Nuts";
+				else if($toppings == "dryfruits")
+					echo "Dry Fruits";
+				?>
+			</td>
+			<td>1</td>
+			<td>
+				<?php 
+					 if($toppings == "nuts")
+						echo 30;
+					else if($toppings == "dryfruits")
+						echo 35;
+				
+				?>
+			</td>
+		</tr>
+
+	</table>
+</body>
+</html>
